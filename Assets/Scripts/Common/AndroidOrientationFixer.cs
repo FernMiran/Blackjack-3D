d72e8880ca63257d544864de3f61aedf65a596e5
FernMiran <fernando.miranda.delgado@gmail.com>
2024-10-25 13:17:01 -0600

    Reworked on the Android Orientation Fixer
    Renamed Orientation Fixer to Android Fixer, code only executes when on the Android Platform

--- Assets/Scripts/Common/AndroidOrientationFixer.cs
+++ Assets/Scripts/Common/AndroidOrientationFixer.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.Android;
 
 public class OrientationManager : MonoBehaviour
 {
 	void Awake()
 	{
+#if UNITY_ANDROID
+		Debug.Log("Fixing Android rotation");
+
 		// Force landscape mode
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
 
@@ -14,5 +18,6 @@ public class OrientationManager : MonoBehaviour
 		Screen.autorotateToPortraitUpsideDown = false;
 
 		Screen.orientation = ScreenOrientation.AutoRotation;
+#endif
 	}
 }
